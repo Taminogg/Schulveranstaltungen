cc3fabcc66100491835bfbe2b089aa55f575a2e7
Jaster <julian.zecher@feuerregen.at>
2023-10-16 19:41:48 +0200

    updated backend url and added secure backend url

--- frontend/apps/frontend/src/environments/environment.prod.ts
+++ frontend/apps/frontend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 ﻿export const environment = {
   production: true,
   backend: document.location.origin.replace(/\/[\w-]+\.plugin/, '/plugin.backend'),
+  secureBackend: document.location.origin.replace(/\/[\w-]+\.plugin/, '/plugin.secureBackend'),
   devToken: ''
 };
